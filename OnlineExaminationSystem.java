69ea1c619e79c947fd7b37db82fbe9a524b2ca28
Abuzar <117680804+abuzar2091@users.noreply.github.com>
2023-06-27 19:00:07 +0530

    Update OnlineExaminationSystem

--- OnlineExaminationSystem.java
+++ OnlineExaminationSystem.java
@@ -1,75 +1,154 @@
 import java.util.*;
 
-public class OnlineExaminationSystem {
+class User {
 
-    private HashMap<String, List<Question>> questions;
+    String username;
+    String password;
 
-    public OnlineExaminationSystem() {
-        this.questions = new HashMap<>();
+    User(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+}
+
+class Examination {
+    User currentUser;
+    int remainingTime;
+
+    Examination(User user) {
+        this.currentUser = user;
+        this.remainingTime = 0;
     }
 
-    public void addQuestion(String subject, Question question) {
-        if (!questions.containsKey(subject)) {
-            questions.put(subject, new ArrayList<>());
-        }
-        questions.get(subject).add(question);
-    }
-
-    public List<Question> getQuestions(String subject) {
-        return questions.get(subject);
-    }
-
-    public static void main(String[] args) {
+    void login() {
         Scanner scanner = new Scanner(System.in);
 
-        OnlineExaminationSystem examinationSystem = new OnlineExaminationSystem();
+        System.out.print("Enter Username: ");
+        String username = scanner.nextLine();
 
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+
+        if (username.equals(currentUser.username) && password.equals(currentUser.password)) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("You Entered Invalid credentials. Login failed!");
+            System.out.println("Try Again With valid credentials");
+            login();
+
+        }
+    }
+
+    void startTimer(int timeLimit) {
+        remainingTime = timeLimit;
+        System.out.println("You have " + remainingTime / 1000 + " seconds for 10 MCQ");
+
+        try {
+            Thread.sleep(remainingTime / 2);
+            System.out.println("Remaining time: " + remainingTime / 2000 + " seconds");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        remainingTime = remainingTime / 2;
+        try {
+            Thread.sleep(remainingTime);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Time's up! Submitting the examination.");
+        try {
+            Thread.sleep(5000);
+            System.out.println("Examination submitted successfully!");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    void closeSession() {
+        System.out.println("Closing session. Goodbye!");
+        // System.exit(0);
+    }
+}
+
+class OnlineExamination {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Are you a new User yes/no ");
+        String reply = sc.next();
+
+        // Perform login
+        String name, password;
+        boolean oldUser = false;
+        if (reply.equals("yes")) {
+            System.out.println("SignUp New Account");
+            System.out.println("Enter Username");
+            name = sc.next();
+            System.out.println("Enter Password");
+            password = sc.next();
+
+            System.out.println("SignUP successful!");
+        } else {
+            name = "Abuzar";
+            password = "12112091";
+            oldUser = true;
+            System.out.println("Login your Account");
+        }
+
+        User user = new User(name, password);
+        Examination examination = new Examination(user);
+        if (oldUser)
+            examination.login();
+        boolean attempt = false;
+        int score = 0;
         while (true) {
             System.out.println("Welcome to the online examination system. What would you like to do?");
             System.out.println("1. Take an exam");
             System.out.println("2. View your results");
             System.out.println("3. Exit");
 
-            int choice = scanner.nextInt();
-
+            int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Enter the subject you would like to take an exam for: ");
-                    String subject = scanner.next();
-
-                    List<Question> questions = examinationSystem.getQuestions(subject);
-
-                    if (questions != null) {
-                        int score = 0;
-                        for (Question question : questions) {
-                            System.out.println(question.getQuestion());
-                            for (String answer : question.getAnswers()) {
-                                System.out.println("* " + answer);
-                            }
-                            System.out.println("Enter your answer: ");
-                            String userAnswer = scanner.next();
-                            if (userAnswer.equals(question.getCorrectAnswer())) {
-                                score++;
-                            }
-                        }
-                        System.out.println("Your score is " + score);
-                    } else {
-                        System.out.println("There are no questions for the selected subject.");
+                    System.out.println("Enter the subject choice you would like to take an exam for: ");
+                    System.out.println("1. Java");
+                    System.out.println("2. C++");
+                    System.out.println("3. Python");
+                    int subject_choice = sc.nextInt();
+                    attempt = true;
+                    switch (subject_choice) {
+                        case 1:
+                            System.out.println(
+                                    "There are 10 MCQ of Java programming Language. There is Negative marking");
+                            score += 100;
+                            break;
+                        case 2:
+                            System.out
+                                    .println("There are 10 MCQ of C++ programming Language. There is Negative marking");
+                            score += 100;
+                            break;
+                        case 3:
+                            System.out.println(
+                                    "There are 10 MCQ of Python programming Language. There is Negative marking");
+                            score += 100;
+                            break;
                     }
+                    examination.startTimer(10000);
+                    examination.closeSession();
+
                     break;
+
                 case 2:
-                    System.out.println("Enter your student ID: ");
-                    String studentId = scanner.next();
-
-                    List<Result> results = examinationSystem.getResults(studentId);
-
-                    if (results != null) {
-                        for (Result result : results) {
-                            System.out.println("Subject: " + result.getSubject());
-                            System.out.println("Score: " + result.getScore());
-                        }
+                    if (!attempt) {
+                        System.out.println("You did not attempt any MCQ exam till now so your score is 0");
                     } else {
-                        System.out.println("You have not taken any exams yet.");
+                        System.out.println("Your score is " + score);
                     }
                     break;
                 case 3:
@@ -80,5 +159,6 @@ public class OnlineExaminationSystem {
                     break;
             }
         }
+
     }
 }
